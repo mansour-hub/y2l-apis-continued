f10fadf117d17976df9390dccfda4a20247dcd69
myname21-meet <myname21@meet.mit.edu>
2019-12-19 19:10:57 +0200

    boat

--- app.py
+++ app.py
@@ -4,36 +4,37 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template('home.html')
+	return render_template('home.html')
 
 @app.route('/study_image', methods = ['POST'])
 def study_image():
 
 	image_url = request.form['url-input']
-    # At this point you have the image_url value from the front end
-    # Your job now is to send this information to the Clarifai API
-    # and read the result, make sure that you read and understand the
-    # example we covered in the slides! 
+	# At this point you have the image_url value from the front end
+	# Your job now is to send this information to the Clarifai API
+	# and read the result, make sure that you read and understand the
+	# example we covered in the slides! 
 
-    # YOUR CODE HERE!
+	# YOUR CODE HERE!
 # you can use the headers to pass in hidden info, here we are sending a secret Key (think of it as a password)
-	headers = {'Authorization': 'key <s2_Mansour>'}
+	headers = {'Authorization': 'Key 214d2af8741946008be0578240fc481d'}
 
 # this is the url of where your request will go
 	api_url = "https://api.clarifai.com/v2/models/aaa03c23b3724a16a56b629203edc62c/outputs"
 
 # this is content of the message(data) you are sending to clarifai
 	data ={"inputs": [
-      	{
-        	"data": {
-          	"image": {
-            	"url": "https://samples.clarifai.com/metro-north.jpg"
-          	}
-        }
-      }
-    ]}
+		{
+			"data": {
+			"image": {
+				"url": image_url
+			}
+		}
+	  }
+	]}
 	response = requests.post(api_url, headers=headers, data=json.dumps(data))
-	return render_template('home.html', results=response.content)
+	response_parsed=json.loads(response.content)
+	return render_template('home.html', results=response_parsed["outputs"][0]["data"]["concepts"])
 
 if __name__ == '__main__':
-    app.run(debug=True)
+	app.run(debug=True)
